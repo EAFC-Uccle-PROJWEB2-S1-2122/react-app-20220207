076a2729cb0a1ff75a6865a5294052ef2cea4361
Francois ROLAND <froland@froland.be>
2022-02-05 15:11:00 +0000

    Style page with CSS.

--- src/App.js
+++ src/App.js
@@ -96,8 +96,6 @@ const App = () => {
         onSearchSubmit={handleSearchSubmit}
       />
 
-      <hr />
-
       {stories.isError && <p>Something went wrong !</p>}
 
       {stories.isLoading ? (
@@ -110,7 +108,7 @@ const App = () => {
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -118,7 +116,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search:</strong>
     </InputWithLabel>
-    <button type="button" disabled={!searchTerm}>
+    <button
+      type="button"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -133,8 +135,16 @@ const InputWithLabel = ({
 }) => {
   return (
     <>
-      <label htmlFor={id}>{children}</label>
-      <input id={id} type={type} value={value} onChange={onInputChange} />
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+        className="input"
+      />
     </>
   );
 };
@@ -148,15 +158,19 @@ const List = ({ list, onRemoveItem }) => (
 );
 
 const Item = ({ item, onRemoveItem }) => (
-  <li>
-    <span>
+  <li className="item">
+    <span style={{ width: "40%" }}>
       <a href={item.url}>{item.title}</a>{" "}
     </span>
-    <span>{item.author} </span>
-    <span>{item.num_comments} </span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
+    <span style={{ width: "30%" }}>{item.author} </span>
+    <span style={{ width: "10%" }}>{item.num_comments} </span>
+    <span style={{ width: "10%" }}>{item.points}</span>
+    <span style={{ width: "10%" }}>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className="button button_small"
+      >
         Dismiss
       </button>
     </span>
